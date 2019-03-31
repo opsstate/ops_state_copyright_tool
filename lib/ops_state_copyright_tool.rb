e75451152870500e5239e0fb8381c074795aed33
Adam Luckey <adamluckey@opsstate.com>
2019-03-30 18:59:21 -0700

    Ops tool initial commit

---
+++ lib/ops_state_copyright_tool.rb
@@ -0,0 +1,7 @@
+require "ops_state_copyright_tool/version"
+require "ops_state_copyright_tool/renderer"
+
+module OpsStateCopyrightTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/ops_state_copyright_tool/version.rb
@@ -0,0 +1,3 @@
+module OpsStateCopyrightTool
+  VERSION = "0.1.0"
+end
